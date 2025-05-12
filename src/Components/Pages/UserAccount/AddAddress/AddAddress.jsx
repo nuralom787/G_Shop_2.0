6159c05fcc91e63711c830e359bcbe368412a144
nuralom787 <md9577630@gmail.com>
2025-05-12 20:39:58 +0600

    update onsubmit functionality and error handiling, also fix some bugs

--- src/Components/Pages/UserAccount/AddAddress/AddAddress.jsx
+++ src/Components/Pages/UserAccount/AddAddress/AddAddress.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import useAxiosPublic from "../../../../Hooks/useAxiosPublic";
 
 const AddAddress = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, control, formState: { errors }, setError } = useForm();
 
     const axiosPublic = useAxiosPublic();
     const [loading, setLoading] = useState(false);
@@ -20,49 +20,61 @@ const AddAddress = () => {
     useEffect(() => {
         axiosPublic.get("/api/v1/region")
             .then(res => {
-                console.log(res.data);
+                // console.log(res.data);
                 setRegions(res.data);
             })
             .catch(err => {
-                console.log(err);
+                // console.log(err);
             })
     }, []);
 
     const handleCityAddress = (e) => {
         setLoading(true);
         setCites([]);
+        setCity("");
         setZones([]);
+        setZone("");
         setRegion(e.target.options[e.target.selectedIndex].text);
         axiosPublic.get(`/api/v1/city?addressId=${e.target.value}`)
             .then(res => {
-                console.log(res.data);
+                // console.log(res.data);
                 setCites(res.data);
                 setLoading(false);
             })
             .catch(err => {
                 setLoading(false);
-                console.log(err);
+                // console.log(err);
             })
     };
 
     const handleZoneAddress = (e) => {
         setLoading(true);
         setZones([]);
+        setZone("");
         setCity(e.target.options[e.target.selectedIndex].text);
         axiosPublic.get(`/api/v1/zone?addressId=${e.target.value}`)
             .then(res => {
-                console.log(res.data);
+                // console.log(res.data);
                 setZones(res.data);
                 setLoading(false);
             })
             .catch(err => {
                 setLoading(false);
-                console.log(err);
+                // console.log(err);
             })
     };
 
 
     const onSubmit = (data) => {
+        if (!city) {
+            return (
+                setError("city"),
+                setError("zone")
+            )
+        }
+        if (!zone) {
+            return setError("zone")
+        }
         data.region = region
         data.city = city
         console.log(data)
@@ -99,7 +111,7 @@ const AddAddress = () => {
                             <p className="text-xs font-medium text-gray-600 my-1.5">Phone Number</p>
                             <input
                                 {...register("phoneNumber", { required: true })}
-                                className={`px-6 py-2 outline-0 border border-gray-400 w-full text-sm ${errors.fullName && "border-red-500"}`}
+                                className={`px-6 py-2 outline-0 border border-gray-400 w-full text-sm ${errors.phoneNumber && "border-red-500"}`}
                                 placeholder="Please enter your phone number"
                                 type="number"
                             />
@@ -107,59 +119,91 @@ const AddAddress = () => {
                         </label>
                         <label>
                             <p className="text-xs font-medium text-gray-600 my-1.5">Province / Region</p>
-                            <select
-                                {...register("region", { required: true })}
-                                onChange={(e) => handleCityAddress(e)}
-                                className={`px-6 py-2 outline-0 border border-gray-400 w-full text-sm ${errors.fullName && "border-red-500"}`}
-                            >
-                                <option hidden>Please Choose your Province/Region</option>
-                                {
-                                    regions.map(region => <option
-                                        key={region.id}
-                                        value={region.id}
+                            <Controller
+                                name="region"
+                                control={control}
+                                rules={{ required: true }}
+                                render={({ field }) => (
+                                    <select
+                                        {...field}
+                                        onChange={(e) => {
+                                            field.onChange(e);
+                                            handleCityAddress(e);
+                                        }}
+                                        className={`px-6 py-2 outline-0 border border-gray-400 w-full text-sm cursor-pointer ${errors.region && "border-red-500"}`}
                                     >
-                                        {region.name}
-                                    </option>)
-                                }
-                            </select>
+                                        <option hidden>Please Choose your Province/Region</option>
+                                        {
+                                            regions.map(region => <option
+                                                key={region.id}
+                                                value={region.id}
+                                            >
+                                                {region.name}
+                                            </option>)
+                                        }
+                                    </select>
+                                )}
+                            />
                             {errors.region && <span className="text-red-500 text-xs font-medium px-2 py-1">You can't leave this empty.</span>}
                         </label>
                         <label>
                             <p className="text-xs font-medium text-gray-600 my-1.5">City</p>
-                            <select
-                                {...register("city", { required: true })}
-                                onChange={(e) => handleZoneAddress(e)}
-                                className={`px-6 py-2 outline-0 border border-gray-400 w-full text-sm ${errors.fullName && "border-red-500"}`}
-                            >
-                                <option hidden>Please Choose your city</option>
-                                {
-                                    cites.map(city => <option
-                                        key={city.id}
-                                        value={city.id}
+                            <Controller
+                                name="city"
+                                control={control}
+                                rules={{ required: true }}
+                                render={({ field }) => (
+                                    <select
+                                        {...field}
+                                        disabled={cites.length ? false : true}
+                                        onChange={(e) => {
+                                            field.onChange(e);
+                                            handleZoneAddress(e);
+                                        }}
+                                        className={`px-6 py-2 outline-0 border border-gray-400 w-full text-sm ${errors.city && "border-red-500"} ${cites.length ? "cursor-pointer" : "cursor-not-allowed"}`}
                                     >
-                                        {city.name}
-                                    </option>)
-                                }
-                            </select>
+                                        <option hidden>Please Choose your city</option>
+                                        {
+                                            cites.map(city => <option
+                                                key={city.id}
+                                                value={city.id}
+                                            >
+                                                {city.name}
+                                            </option>)
+                                        }
+                                    </select>
+                                )}
+                            />
                             {errors.city && <span className="text-red-500 text-xs font-medium px-2 py-1">You can't leave this empty.</span>}
                         </label>
                         <label>
                             <p className="text-xs font-medium text-gray-600 my-1.5">Zone</p>
-                            <select
-                                {...register("zone", { required: true })}
-                                onChange={(e) => setZone(e.target.value)}
-                                className={`px-6 py-2 outline-0 border border-gray-400 w-full text-sm ${errors.fullName && "border-red-500"}`}
-                            >
-                                <option hidden>Please Choose your zone</option>
-                                {
-                                    zones.map(zone => <option
-                                        key={zone.id}
-                                        value={zone.name}
+                            <Controller
+                                name="zone"
+                                control={control}
+                                rules={{ required: true }}
+                                render={({ field }) => (
+                                    <select
+                                        {...field}
+                                        disabled={zones.length ? false : true}
+                                        onChange={(e) => {
+                                            field.onChange(e)
+                                            setZone(e.target.value)
+                                        }}
+                                        className={`px-6 py-2 outline-0 border border-gray-400 w-full text-sm ${errors.zone && "border-red-500"} ${zones.length ? "cursor-pointer" : "cursor-not-allowed"}`}
                                     >
-                                        {zone.name}
-                                    </option>)
-                                }
-                            </select>
+                                        <option hidden>Please Choose your zone</option>
+                                        {
+                                            zones.map(zone => <option
+                                                key={zone.id}
+                                                value={zone.name}
+                                            >
+                                                {zone.name}
+                                            </option>)
+                                        }
+                                    </select>
+                                )}
+                            />
                             {errors.zone && <span className="text-red-500 text-xs font-medium px-2 py-1">You can't leave this empty.</span>}
                         </label>
                     </div>
