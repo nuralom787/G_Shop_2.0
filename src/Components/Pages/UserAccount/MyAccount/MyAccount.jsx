09b359d4ee7f425079334b34234a18c0d9ab3f69
nuralom787 <md9577630@gmail.com>
2025-05-21 20:02:06 +0600

    improve my account page and show address information

--- src/Components/Pages/UserAccount/MyAccount/MyAccount.jsx
+++ src/Components/Pages/UserAccount/MyAccount/MyAccount.jsx
@@ -30,10 +30,11 @@ const MyAccount = () => {
                         </div>
                         :
                         <div>
-                            <h3 className="mb-4 font-semibold text-[#151515] text-lg">
+                            <h3 className="font-semibold text-[#151515] text-lg">
                                 Personal Profile
                                 <span className="font-semibold text-sm text-cyan-600 ms-2 ps-2 border-s-2 border-s-gray-400"><Link to="/user/profile">EDIT</Link></span>
                             </h3>
+                            <div className="divider before:bg-black after:bg-black mt-1.5 mb-3.5 h-[2px]"></div>
                             <p>{account?.displayName}</p>
                             <p>{account?.email?.split('@')[0].slice(0, 2) + '*'.repeat(account?.email?.split('@')[0].length - 2) + '@' + account?.email?.split('@')[1]}</p>
                             <p>{account?.phoneNumber}</p>
@@ -53,21 +54,51 @@ const MyAccount = () => {
                         </div>
                         :
                         <div>
-                            <h3 className="mb-4 font-semibold text-[#151515] text-lg">
+                            <h3 className="font-semibold text-[#151515] text-lg">
                                 Address Book
-                                <span className="font-semibold text-sm text-cyan-600 ms-2 ps-2 border-s-2 border-s-gray-400"><Link to="/user/addresses">ADD</Link></span>
+                                <span className="font-semibold text-sm text-cyan-600 ms-2 ps-2 border-s-2 border-s-gray-400">
+                                    <Link to="/user/addresses">ADD</Link>
+                                </span>
                             </h3>
-                            <div className="flex flex-col md:flex-row">
-                                <div className="grow">
-                                    <p className="text-sm font-light text-gray-400">Save your shipping address here.</p>
-                                    <div className="my-4">
-                                        <LuMapPin className="text-4xl text-gray-400" />
+                            <div className="divider before:bg-black after:bg-black mt-1.5 mb-3.5 h-[2px]"></div>
+                            {account.addresses.length ?
+                                <div>
+                                    {
+                                        account.addresses.slice(0, 1).map(address => <div key={address._id} className="flex flex-col md:flex-row">
+                                            <div className="grow space-y-1">
+                                                <p className="text-sm text-gray-400">DEFAULT SHIPPING ADDRESS</p>
+                                                <h3 className="font-semibold text-base">{address.fullName}</h3>
+                                                <p className="text-sm">
+                                                    {address.address}
+                                                    <span className="text-sm block">{address.region}, {address.city}, {address.zone}.</span>
+                                                </p>
+                                                <span>(+88) {address.phoneNumber}</span>
+                                            </div>
+                                            <div className="ps-3 border-s border-s-gray-300 grow space-y-1">
+                                                <p className="text-sm text-gray-400">DEFAULT BILLING ADDRESS</p>
+                                                <h3 className="font-semibold text-base">{address.fullName}</h3>
+                                                <p className="text-sm">
+                                                    {address.address}
+                                                    <span className="text-sm block">{address.region}, {address.city}, {address.zone}.</span>
+                                                </p>
+                                                <span>(+88) {address.phoneNumber}</span>
+                                            </div>
+                                        </div>)
+                                    }
+                                </div>
+                                :
+                                <div className="flex flex-col md:flex-row">
+                                    <div className="grow">
+                                        <p className="text-sm font-light text-gray-400">Save your shipping address here.</p>
+                                        <div className="my-4">
+                                            <LuMapPin className="text-4xl text-gray-400" />
+                                        </div>
+                                    </div>
+                                    <div className="ps-3 border-s border-s-gray-300 grow">
+                                        <p className="text-sm font-light text-gray-400">Save your billing address here.</p>
                                     </div>
                                 </div>
-                                <div className="ps-3 border-s border-s-gray-300 grow">
-                                    <p className="text-sm font-light text-gray-400">Save your billing address here.</p>
-                                </div>
-                            </div>
+                            }
                         </div>
                     }
                 </div>
